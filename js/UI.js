0b05a1e14324131ce5be5c78741ef6049fdf846c
Mohammed Kamaldeen <mohammedkamaldeen204@gmail.com>
2022-08-16 15:03:51 +0100

    done with unit converter mass and length

--- js/UI.js
+++ js/UI.js
@@ -1,13 +1,11 @@
 $(document).ready(function() {
 
-
-
 $(".normcalc").click(function() {
     $(".scientific-func").hide()
     $(".scicalc").removeClass("let1")
     $(".normcalc").addClass("let1")
     $(".converter").hide();
-    $(".science2").show();
+    $(".NORMAL").show();
 });
 
 $(".scicalc").click(function() {
@@ -16,32 +14,35 @@ $(".scicalc").click(function() {
     $(".normcalc").removeClass("let1")
     $(".normcalc").addClass("let")
     $(".converter").hide();
-    $(".science2").show();
+    $(".NORMAL").show();
 });
 
 $(".unit").click(function(){
     $(".scientific-func").hide();
-    $(".science2").hide();
+    $(".NORMAL").hide();
     $(".converter").show();
     $(".scicalc").removeClass("let1")
+    $(".normcalc").removeClass("let1")
 })
 
 // CODE FOR DARK
 $(".dark-mode").click(function(){
+ 
    $("form").addClass("back")
    $(".noir").addClass("inputfd")
    $(".noir1").addClass("inputfd1")
    $(".noir").removeClass("input")
    $(".noir1").removeClass("input1")
-   $(".rowd").addClass("rowdark")
+   $(".rowd2").addClass("rowdark")
    $(".roww").addClass("rowdark")
    $(".call").addClass("output1")
    $(".inputbt").addClass("inputbtd")
    $(".input13").addClass("inputd")
    $(".input13").removeClass("inputc")
    $(".inputbt").removeClass("inputbtn")
+   $(".light-mode").show();
    $(".dark-mode").hide()
-   $(".light-mode").show()
+   $(".rowd2").show()
 })
 
 
@@ -52,14 +53,44 @@ $(".light-mode").click(function(){
    $(".noir1").removeClass("inputfd1")
    $(".noir").addClass("input")
    $(".noir1").addClass("input1")
-   $(".rowd").removeClass("rowdark")
+   $(".rowd2").removeClass("rowdark")
    $(".roww").removeClass("rowdark")
    $(".call").removeClass("output1")
    $(".inputbt").removeClass("inputbtd")
    $(".input13").removeClass("inputd")
    $(".input13").addClass("inputc")
    $(".inputbt").addClass("inputbtn")
+   $(".light-mode").hide();
    $(".dark-mode").show()
-   $(".light-mode").hide()
+   $(".rowd").show()
+   $(".rowd2").hide()
 })
+
+
+// MASS BUTTON 
+$(".massbtn").click(function(){
+$("section.mass").show();
+$(".scientific-func").hide();
+$(".NORMAL").hide();
+$(".converter").hide();
+
+})
+
+// LENGTH BUTTON
+$(".lengthbtn").click(function(){
+    $("section.length").show();
+    $(".scientific-func").hide();
+    $(".NORMAL").hide();
+    $(".converter").hide();
+    
+    })
+
+    // BACK BUTTON
+$(".backmass").click(function(){
+    $(".converter").show();
+    $("section.mass").hide();
+    $("section.length").hide();
+})
+
+
 });
--- js/science.js
+++ js/science.js
@@ -6,7 +6,7 @@ function back() {
 }
 
 function square(form) {
-	form.display2.value = eval(form.display.value) * eval(form.display.value)
+    form.display2.value = eval(form.display.value) * eval(form.display.value)
     form.display.value = eval(form.display.value) * eval(form.display.value)
 }
 
@@ -16,14 +16,14 @@ function cube(form) {
 }
 
 function sqrt(arg) {
-let num = Math.sqrt(arg)
-return num;
+    let num = Math.sqrt(arg)
+    return num;
 }
 
 function cbrt(arg) {
     let num = Math.cbrt(arg)
     return num;
-    }
+}
 
 // function factorial(form) {
 //     arg = form.display.value.length;
@@ -35,52 +35,52 @@ function cbrt(arg) {
 //     }
 // }
 function factorial(n) {
-  let answer = 1
-  if (n == 0 || n==1 ) {
-    return answer;
-  } else{
-    for (let i = n; i >=1; i--) {
-        answer = answer * i;
+    let answer = 1
+    if (n == 0 || n == 1) {
+        return answer;
+    } else {
+        for (let i = n; i >= 1; i--) {
+            answer = answer * i;
+        }
+        return answer;
     }
-    return answer;
-  }
 }
-function calc_deg(){
+function calc_deg() {
     var element = document.getElementById("deg")
     element.style = "border: solid red"
     var element2 = document.getElementById("rad")
     element2.style = "border none"
-    
+
     selection = "deg" //Set selection to deg
-    
-  }
-  
-  function calc_rad(){
+
+}
+
+function calc_rad() {
     var element2 = document.getElementById("rad")
     element2.style = "border: solid red"
     var element = document.getElementById("deg")
     element.style = "border: none"
-    
+
     selection = "rad" //Set selection to rad
-    
-  }
+
+}
 
 function sin(arg) {
-if (selection == "rad") {
-    let num = Math.sin(arg)
-    return num;
-} else if (selection == "deg") {
- let num = Math.round(Math.sin(arg * Math.PI / 180) * 10) / 10;
-    return num
-}
+    if (selection == "rad") {
+        let num = Math.sin(arg)
+        return num;
+    } else if (selection == "deg") {
+        let num = Math.round(Math.sin(arg * Math.PI / 180) * 10) / 10;
+        return num
+    }
 }
 function cos(arg) {
     if (selection == "rad") {
         let num = Math.cos(arg)
         return num;
     } else if (selection == "deg") {
-    let num = Math.round(Math.cos(arg * Math.PI / 180) * 10) / 10;
-    return num
+        let num = Math.round(Math.cos(arg * Math.PI / 180) * 10) / 10;
+        return num
     }
 }
 function tan(arg) {
@@ -88,8 +88,8 @@ function tan(arg) {
         let num = Math.tan(arg)
         return num;
     } else if (selection == "deg") {
-    let num = Math.round(Math.tan(arg * Math.PI / 180) * 10) / 10;
-    return num
+        let num = Math.round(Math.tan(arg * Math.PI / 180) * 10) / 10;
+        return num
     }
 }
 function asin(arg) {
@@ -97,8 +97,8 @@ function asin(arg) {
         let num = Math.asin(arg)
         return num;
     } else if (selection == "deg") {
-    let num = Math.floor(Math.asin(arg )* 180/Math.PI)
-    return num
+        let num = Math.floor(Math.asin(arg) * 180 / Math.PI)
+        return num
     }
 }
 function acos(arg) {
@@ -106,8 +106,8 @@ function acos(arg) {
         let num = Math.acos(arg)
         return num;
     } else if (selection == "deg") {
-    let num = Math.floor(Math.acos(arg )* 180/Math.PI)
-    return num
+        let num = Math.floor(Math.acos(arg) * 180 / Math.PI)
+        return num
     }
 }
 function atan(arg) {
@@ -115,8 +115,8 @@ function atan(arg) {
         let num = Math.atan(arg)
         return num;
     } else if (selection == "deg") {
-    let num = Math.floor(Math.atan(arg )* 180/Math.PI)
-    return num
+        let num = Math.floor(Math.atan(arg) * 180 / Math.PI)
+        return num
     }
 }
 function checkNum(Number) {
@@ -128,7 +128,7 @@ function checkNum(Number) {
                 return false
             }
         }
-    } 
+    }
     return true;
 }
 
@@ -145,4 +145,120 @@ function exp(arg) {
 function random() {
     let num = Math.random()
     return num;
+}
+function gram(number1) {
+
+    $(".num2").innerHTML = number1 / 1000
+
+}
+function massConverter(valNum) {
+    let selection = $("select#inputGroupSelect01").val()
+    if (selection === "empty") {
+        $(".ifempty").show();
+    }
+
+    else if (selection === "Gram") {
+        $(".gram-gram").hide()
+        $(".kilo-gram").show()
+        document.getElementById("num2").innerHTML = valNum / 1000;
+    } else {
+        document.getElementById("num2").innerHTML = valNum * 1000;
+        $(".kilo-gram").hide();
+        $(".gram-gram").show();
+    }
+}
+
+function lengthConverter(valNum) {
+    let selection1 = $("select#inputGroupSelect02").val()
+    let selection2 = $("select#inputGroupSelect022").val()
+    if (selection1 === "empty" && selection2 === "empty") {
+        $(".ifempty").show();
+    }
+
+    else if (selection1 === "centimeter" && selection2 === "meter") {
+        $(".meter-meter").show()
+        $(".kilo-metre").hide()
+        $(".mile-mile").hide()
+        $(".centi-meter").hide()
+        document.getElementById("num3").innerHTML = valNum / 100;
+    } else if (selection1 === "centimeter" && selection2 === "kilometre") {
+        $(".kilo-metre").show()
+        $(".mile-mile").hide()
+        $(".centi-meter").hide()
+        $(".meter-meter").hide()
+        document.getElementById("num3").innerHTML = valNum / 100000;
+    }
+    else if (selection1 === "centimeter" && selection2 === "mile") {
+        $(".mile-mile").show()
+        $(".kilo-metre").hide()
+        $(".centi-meter").hide()
+        $(".meter-meter").hide()
+        document.getElementById("num3").innerHTML = valNum / 160900;
+    }
+    else if (selection1 === "meter" && selection2 === "centimeter") {
+        $(".centi-meter").show()
+        $(".kilo-metre").hide()
+        $(".mile-mile").hide()
+        $(".meter-meter").hide()
+        document.getElementById("num3").innerHTML = valNum * 100;
+    } else if (selection1 === "meter" && selection2 === "kilometre") {
+        $(".centi-meter").hide()
+        $(".kilo-metre").show()
+        $(".mile-mile").hide()
+        $(".meter-meter").hide()
+        document.getElementById("num3").innerHTML = valNum / 1000;
+    } else if (selection1 === "meter" && selection2 === "mile") {
+        $(".centi-meter").hide()
+        $(".kilo-metre").hide()
+        $(".mile-mile").show()
+        $(".meter-meter").hide()
+        document.getElementById("num3").innerHTML = valNum / 1609;
+    } else if (selection1 === "kilometre" && selection2 === "centimeter") {
+        $(".centi-meter").show()
+        $(".kilo-metre").hide()
+        $(".mile-mile").hide()
+        $(".meter-meter").hide()
+        document.getElementById("num3").innerHTML = valNum * 100000;
+    } else if (selection1 === "kilometre" && selection2 === "meter") {
+        $(".centi-meter").hide()
+        $(".kilo-metre").hide()
+        $(".mile-mile").hide()
+        $(".meter-meter").show()
+        document.getElementById("num3").innerHTML = valNum * 1000;
+    } else if (selection1 === "kilometre" && selection2 === "mile") {
+        $(".centi-meter").hide()
+        $(".kilo-metre").hide()
+        $(".mile-mile").show()
+        $(".meter-meter").hide()
+        document.getElementById("num3").innerHTML = valNum * 1.609;
+    } else if (selection1 === "mile" && selection2 === "centimeter") {
+        $(".centi-meter").show()
+        $(".kilo-metre").hide()
+        $(".mile-mile").hide()
+        $(".meter-meter").hide()
+        document.getElementById("num3").innerHTML = valNum * 160900;
+    } else if (selection1 === "mile" && selection2 === "meter") {
+        $(".centi-meter").hide()
+        $(".kilo-metre").hide()
+        $(".mile-mile").hide()
+        $(".meter-meter").show()
+        document.getElementById("num3").innerHTML = valNum * 1609;
+    } else if (selection1 === "mile" && selection2 === "kilometre") {
+        $(".centi-meter").hide()
+        $(".kilo-metre").show()
+        $(".mile-mile").hide()
+        $(".meter-meter").hide()
+         $(".sameunit").hide();
+         $(".sameunit").show();
+        document.getElementById("num3").innerHTML = valNum * 1.609;
+    }
+    else {
+       $(".sameunit").show();
+       $(".ifempty").hide();
+       $(".centi-meter").hide()
+        $(".kilo-metre").hide()
+        $(".mile-mile").hide()
+        $(".meter-meter").hide()
+       
+    }
 }
